81e167793bfc5c0c9c8ecfd4dd4536b183918491
Gaurav Bajaj <GauravBajaj@Gauravs-MacBook-Pro.local>
2015-05-06 17:28:56 -0700

    deleted everything

--- src/models/row-model.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require("mongoose");
- Schema = mongoose.Schema;
- var RowSchema = new Schema({
- 	field: {type: String},
-    value: {type: String}
- });
- module.exports = mongoose.model('Row', RowSchema);
